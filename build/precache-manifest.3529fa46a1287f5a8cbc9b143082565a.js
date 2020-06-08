f82119c8cf09920c672dad3e7283255e02ed4af8
Evgeny <evgeny.paromov@gmail.com>
2020-06-08 10:35:22 +0300

    added build in git

---
+++ build/precache-manifest.3529fa46a1287f5a8cbc9b143082565a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2a5b54890faaadb6ec64cd624e3fcc",
+    "url": "/ss-admin/index.html"
+  },
+  {
+    "revision": "2ee4224bf89b7c00157c",
+    "url": "/ss-admin/static/css/2.16b06970.chunk.css"
+  },
+  {
+    "revision": "a72073fbf93340834043",
+    "url": "/ss-admin/static/css/main.9b666114.chunk.css"
+  },
+  {
+    "revision": "2ee4224bf89b7c00157c",
+    "url": "/ss-admin/static/js/2.13120bb2.chunk.js"
+  },
+  {
+    "revision": "479f0a747c5a85c6dbe59df8d7f347b2",
+    "url": "/ss-admin/static/js/2.13120bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72073fbf93340834043",
+    "url": "/ss-admin/static/js/main.2e024d24.chunk.js"
+  },
+  {
+    "revision": "37294784511348c38a2d",
+    "url": "/ss-admin/static/js/runtime-main.6ea9b289.js"
+  },
+  {
+    "revision": "07551a2d2e9a2d8b15884b163b1b4178",
+    "url": "/ss-admin/static/media/ice.07551a2d.jpg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/ss-admin/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/ss-admin/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/ss-admin/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/ss-admin/static/media/notification.c0d3c94c.eot"
+  }
+]);
